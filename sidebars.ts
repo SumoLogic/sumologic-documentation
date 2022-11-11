63077bb865b185b3156fe09b6d55d61bf2c2e7e5
megha jain <mjain@sumologic.com>
2022-11-11 18:57:16 +0530

    added azure event hubs files

--- sidebars.ts
+++ sidebars.ts
@@ -169,6 +169,8 @@ module.exports = {
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/1password-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/akamai-siem-api-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/azure-event-hubs-source',
+                'send-data/hosted-collectors/cloud-to-cloud-integration-framework/azure-event-hubs-source/azure-event-hubs-source',
+                'send-data/hosted-collectors/cloud-to-cloud-integration-framework/azure-event-hubs-source/event-hub-cloud-to-cloud-source-migration',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/box-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/carbon-black-cloud-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/carbon-black-inventory-source',
