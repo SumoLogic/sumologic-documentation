9ff7da521c142cb30f12ca88a4d863f9d5506bcf
Kim <56411016+kimsauce@users.noreply.github.com>
2023-01-27 21:27:16 -0800

    edit

--- src/helper/feedback.js
+++ src/helper/feedback.js
@@ -1,2 +1,5 @@
+import React from 'react';
+
 window.feedbackfin = { config: {}, ...window.feedbackfin };
-window.feedbackfin.config.url = "https://rowy-hooks.run.app/wh/...";
+// TODO: place your webhook url here
+window.feedbackfin.config.url = "https://rowy-hooks-2im5zpb7jq-wl.a.run.app/wh/fin/DLZptXS9UYjFhMdaAX2X";
