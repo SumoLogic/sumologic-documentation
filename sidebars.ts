8331af9f23fe291f216eeca19708240f89220b10
Keyur Shah <119590000+keyur-sumo@users.noreply.github.com>
2023-03-20 11:32:29 +0530

    [CON-1636] Create DocuSign Monitor source doc (#1855)
    * created public doc for docusign cloud-to-cloud connector
    
    * Update docs/send-data/hosted-collectors/cloud-to-cloud-integration-framework/docusign-source.md
    
    * Fixed review comments
    
    * fixed review comments, added border to images
    
    * Update docs/send-data/hosted-collectors/cloud-to-cloud-integration-framework/docusign-source.md
    
    * Update docusign-source.md
    
    Fixed broken formatting under "App" header
    
    * Update docs/send-data/hosted-collectors/cloud-to-cloud-integration-framework/docusign-source.md
    
    * Update docs/send-data/hosted-collectors/cloud-to-cloud-integration-framework/docusign-source.md
    
    * Update docs/send-data/hosted-collectors/cloud-to-cloud-integration-framework/docusign-source.md
    
    ---------
    
    Co-authored-by: Kim <56411016+kimsauce@users.noreply.github.com>
    Co-authored-by: Megha Jain <116072777+JainM6@users.noreply.github.com>
    Co-authored-by: Stacy Kornluebke <stacy@sumologic.com>

--- sidebars.ts
+++ sidebars.ts
@@ -192,6 +192,7 @@ module.exports = {
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/cse-aws-ec-inventory-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/cyberark-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/cybereason-source',
+                'send-data/hosted-collectors/cloud-to-cloud-integration-framework/docusign-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/dropbox-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/duo-source',
                 'send-data/hosted-collectors/cloud-to-cloud-integration-framework/gmail-tracelogs-source',
