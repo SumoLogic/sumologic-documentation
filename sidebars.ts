2a8f7e2f24e3ef9607f1c73715b35bea0a965789
Jagadisha V <jv.ctr@sumologic.com>
2023-05-18 15:36:14 +0530

    SQL server docs added

--- sidebars.ts
+++ sidebars.ts
@@ -1775,7 +1775,8 @@ integrations: [
            collapsed: true,
            items: [
              'integrations/microsoft-azure/opentelemetry/active-directory-json-opentelemetry',
-             'integrations/microsoft-azure/opentelemetry/sql-server-opentelemetry',
+             'integrations/microsoft-azure/opentelemetry/sql-server-linux-opentelemetry',
+             'integrations/microsoft-azure/opentelemetry/sql-server-windows-opentelemetry',
            ],
          },
          'integrations/microsoft-azure/active-directory-json',
