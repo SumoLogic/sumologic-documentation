7abeb6cf6767cb87c16803548eaf019a909a3399
Kim <56411016+kimsauce@users.noreply.github.com>
2023-05-27 11:07:49 -0700

    css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,6 +136,7 @@ module.exports = {
         specs: [
           {
             spec: 'sumologic-api.yaml',
+            //specUrl: 'https://api.sumologic.com/docs/sumologic-api.yaml',
             route: '/api/',
           },
         ],
