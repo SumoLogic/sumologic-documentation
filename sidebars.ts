fef705c38aeb225eac743f6fe1d8e917458350d7
Himanshu Pal <hp.iiita@gmail.com>
2023-07-19 14:01:57 +0530

    strimzi kafka app documentation complete (#2791)
    * strimzi kafka app documentation complete
    
    * Grammar fixes in strimzi-kafka.md

--- sidebars.ts
+++ sidebars.ts
@@ -1970,6 +1970,7 @@ integrations: [
           'integrations/containers-orchestration/docker-community-edition',
           'integrations/containers-orchestration/docker-ulm',
           'integrations/containers-orchestration/kafka',
+          'integrations/containers-orchestration/strimzi-kafka',
           'integrations/containers-orchestration/kubernetes',
           'integrations/containers-orchestration/kubernetes-control-plane',
           'integrations/containers-orchestration/rabbitmq',
