c06ebe838a2605926929fe84a6f45f765d2d4549
Carlos Castillo <ccastillo@sumologic.com>
2024-03-11 15:58:19 -0500

    Add TAXII 1 C2C Source

---
+++ static/files/c2c/taxii-1/example.tf
@@ -0,0 +1,20 @@
+resource "sumologic_cloud_to_cloud_source" "taxii2_source" {
+  collector_id = sumologic_collector.collector.id
+  schema_ref = {
+    type = "TAXII 2 Client"
+  }
+  config = jsonencode({
+			"name":"STIX TAXII 1 Client for Unit 42",
+			"description":"Collects indicators from Unit 42 and stores them in Sumo Logic",
+			"category":"source_category",
+            "userSourceID": "My Important Indicators",
+            "discoveryURL": "https://stix2.unit42.org/taxii/",
+            "useBasicAuth: true,
+            "http_user": "user@acme.com",
+            "http_password": "****************************"
+  })
+}
+resource "sumologic_collector" "collector" {
+  name        = "my-collector"
+  description = "Just testing this"
+}
