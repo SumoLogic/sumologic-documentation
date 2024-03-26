ed60df4d17de8f16b15e3dbe50b5b1d6d338cb3c
Jagadisha V <129049263+JV0812@users.noreply.github.com>
2024-03-26 11:01:32 +0530

    Estimate scan data doc added (#3855)
    * Estimate scan data doc added
    
    * minor fix
    
    * minor fix
    
    * Update docs/manage/partitions/flex/estimate-scan-data.md
    
    Co-authored-by: John Pipkin (Sumo Logic) <jpipkin@sumologic.com>
    
    * Update estimate-scan-data.md
    
    * Update estimate-scan-data.md
    
    * Estimate scan data doc added
    
    * minor fix
    
    * minor fix
    
    * Update docs/manage/partitions/flex/estimate-scan-data.md
    
    Co-authored-by: John Pipkin (Sumo Logic) <jpipkin@sumologic.com>
    
    * Update estimate-scan-data.md
    
    * Update estimate-scan-data.md
    
    * screenshots updated
    
    ---------
    
    Co-authored-by: John Pipkin (Sumo Logic) <jpipkin@sumologic.com>
    Co-authored-by: Kim <56411016+kimsauce@users.noreply.github.com>

--- sidebars.ts
+++ sidebars.ts
@@ -671,7 +671,7 @@ module.exports = {
           items: [
             'manage/partitions/flex/create-edit-partition-flex',
             'manage/partitions/flex/view-partition-details-flex',
-            //'manage/partitions/flex/estimate-and-actual-scan-data',
+            'manage/partitions/flex/estimate-scan-data',
             'manage/partitions/flex/flex-pricing-faq',
           ]
         },
