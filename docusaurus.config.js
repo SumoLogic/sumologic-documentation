7c2b81741229a732fbbdf9f0d9b3347731f3773c
Kim <56411016+kimsauce@users.noreply.github.com>
2024-04-10 14:30:36 -0700

    Add RSAC banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -235,6 +235,12 @@ module.exports = {
       },
     // SEO Global Metadata
     metadata: [{name: 'keywords', content: 'sumo logic, documentation, tutorials, quickstarts'}],
+    announcementBar: {
+      id: 'announcementBar',
+      content: `RSAC 2024 - it’s game on for AI-driven DevSecOps - meet us at booth #6271 on May 6–9. 👉<a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSeCdX9T4-ksh_LhCAu7_C1SG3WDnjTgoV1Tgy8VoxwAankcQw/viewform">Book a meeting</a>.`,
+      backgroundColor: '#2f0f3d',
+      textColor: '#fff',
+    },
     imageZoom: {
       selector: '.markdown :not(a) > img',
       // Optional medium-zoom options
