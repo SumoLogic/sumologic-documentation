e6d85c68d713742295aa410e8f16df932de7fb16
Jagadisha V <129049263+JV0812@users.noreply.github.com>
2024-07-16 13:33:08 +0530

    Atlassian c2c Source

---
+++ static/files/c2c/atlassian/example.tf
@@ -0,0 +1,24 @@
+resource "sumologic_cloud_to_cloud_source" "Atlassian_source" {
+  collector_id = sumologic_collector.collector.id
+  schema_ref = {
+    type = "Atlassian"
+  }
+  config = jsonencode({
+            "name": "Atlassian Test",
+            "initialLookback": "1d",
+            "fields": {
+                "_siemForward": true
+            },
+            "pollingInterval": "1h",
+            "organizations": [
+                {
+                    "orgID": "**************",
+                    "token": "*******"
+                }
+            ]
+        })
+}
+resource "sumologic_collector" "collector" {
+  name        = "my-collector"
+  description = "Just testing this"
+}
