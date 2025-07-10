bdd0bc07e420f69c51de8d288ace53a8333e61e7
Amee Lepcha <amee.lepcha.ctr@sumologic.com>
2025-07-10 17:13:38 +0530

    Google Cloud Security Command Center (Apps)

--- sidebars.ts
+++ sidebars.ts
@@ -2282,6 +2282,7 @@ integrations: [
           'integrations/google/cloud-pub-sub',
           'integrations/google/cloud-router',
           'integrations/google/cloud-run',
+          'integrations/google/cloud-security-command-center',
           'integrations/google/cloud-spanner',
           'integrations/google/cloud-tasks',
           'integrations/google/cloud-tpu',
