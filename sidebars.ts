a348669358c84573e7d51464ceab7ec48b1f641a
Jagadisha V <129049263+JV0812@users.noreply.github.com>
2025-08-29 12:53:55 +0530

    Update sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -989,7 +989,6 @@ module.exports = {
             'manage/manage-subscription/create-and-manage-orgs/create-manage-orgs-flex',
             'manage/manage-subscription/create-and-manage-orgs/manage-org-settings',
             'manage/manage-subscription/create-and-manage-orgs/manage-orgs-for-mssps-csiem-rules',
-            'manage/manage-subscription/create-and-manage-orgs/manage-orgs-for-mssps',
           ],
         },
         {
