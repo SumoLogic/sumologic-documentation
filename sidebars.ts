57757afe0abdba92b052b0aede832468c46fc0aa
Amee Lepcha <amee.lepcha.ctr@sumologic.com>
2025-10-27 20:11:16 +0530

    ChatGPT Compliance (apps) (#5962)
    * ChatGPT Compliance (apps)
    
    * Added to Product list
    
    * Update chatgpt-compliance.md
    
    * Update index.md
    
    * changed release date
    
    ---------
    
    Co-authored-by: Priyansh Patel <114739257+priyansh-sumo@users.noreply.github.com>

--- sidebars.ts
+++ sidebars.ts
@@ -2563,6 +2563,7 @@ integrations: [
           'integrations/saas-cloud/box',
           'integrations/saas-cloud/carbon-black-inventory',
           'integrations/saas-cloud/cato-networks',
+          'integrations/saas-cloud/chatgpt-compliance',
           'integrations/saas-cloud/cisco-amp',
           'integrations/saas-cloud/cisco-meraki-c2c',
           'integrations/saas-cloud/cisco-umbrella',
